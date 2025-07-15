bba36cbb55f90bcabf3ffb7504c22f4b8eb9bfd7
Amit Jha <athemastermind47@gmail.com>
2025-07-16 01:42:42 +0200

    Setting up Mortgage App

---
+++ src/test/java/com/ing/assessment/mortgage/MortgageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.assessment.mortgage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
